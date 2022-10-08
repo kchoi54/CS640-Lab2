a4394f6114c1ce018828613c3eb2853c097f40fa
Harshita Singh <harshitasingh47@gmail.com>
2022-10-08 14:48:30 -0500

    Switch table construction

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,12 +4,78 @@ import net.floodlightcontroller.packet.Ethernet;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import java.lang.Runnable;
+import java.lang.Thread;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 /**
  * @author Aaron Gember-Jacobson
+ * 
+ * Edited : Harshita Singh
  */
-public class Switch extends Device
+public class Switch extends Device implements Runnable
 {	
+
+
+	//switch element class definition
+	class SwitchElement{
+		private long lastUpdate;
+		private Iface outInterface; 
+
+
+		public SwitchElement(long lastUpdate, Iface outInterface) {
+			this.lastUpdate = lastUpdate;
+			this.outInterface = outInterface;
+		}
+	
+		public void setLastUpdate(long lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+	
+		public void setoutInterface(Iface outInterface) {
+			this.outInterface = outInterface;
+		}
+	
+		public long getLastUpdate() {
+			return this.lastUpdate;
+		}
+	
+		public Iface getoutInterface() {
+			return this.outInterface;
+		}
+	}
+
+	private ConcurrentHashMap<String, SwitchElement> switchMap; 
+	private Thread t; 
+
+
+	//monitoring the timeout for all elements, removing from table if timedout
+	public void run(){
+		try {
+
+			while(){
+				if(switchMap!=null){
+					for(Map.Entry<String, SwitchElement> entry : switchMap.entrySet()){
+						long timeremaining = System.currentTimeMillis() - entry.getValue().getLastUpdate();
+						if(timeremaining >= 15000L){
+							switchMap.remove(entry.getKey());
+							System.out.println("Entry Timeout - "+ entry.getValue(()));
+						}
+
+
+					}
+
+				}
+				Thread.sleep(200);
+			}
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace(System.out);
+			//TODO: handle exception
+		}
+	}
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -17,6 +83,9 @@ public class Switch extends Device
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+		switchMap = new ConcurrentHashMap<String, SwitchElement>();
+		t = new Thread(this);
+		t.start();
 	}
 
 	/**
@@ -33,5 +102,44 @@ public class Switch extends Device
 		/* TODO: Handle packets                                             */
 		
 		/********************************************************************/
+
+		String sourceMAC = etherPacket.getSourceMAC().toString();
+		String destinationMAC = etherPacket.getDestinationMAC().toString();
+		SwitchElement entry = switchMap.get(destinationMAC);
+
+		// No entry in the table for this destination --> broadcast to all interfaces 
+		if(entry==null){
+
+			System.out.println("Broadcasting to all interfaces...");
+			for(Iface interf : interfaces.values()){
+				if(!inIface.equals(interf)){
+					sendPacket(etherPacket,interf);
+				}
+			}
+
+		}
+
+		//Entry found -> forward to correct interface
+		else{
+			System.out.println("Sending packet...");
+			sendPacket(etherPacket,entry.getOutInterface());
+
+		}
+
+		//Check if switchmap contains information about the current source - if not - populate 
+
+		if(switchMap.containKey(sourceMAC)){
+			System.out.println("Updating the forwarding table...");
+			SwitchElement se  = switchMap.get(sourceMAC);
+			se.setLastUpdate(System.currentTimeMillis());
+			se.setoutInterface(inIface);
+		}
+		else{
+			System.out.println("Adding entry to the forwarding table...");
+			switchMap.put(sourceMAC, new SwitchElement(System.currentTimeMillis(), inIface));
+		}
+
+
+
 	}
 }
